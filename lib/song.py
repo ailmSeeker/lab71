924752afcc113cf07d33f2b18c0da035c278b251
Mohamed Yusuf <mohamed208.y@proton.me>
2023-08-22 11:59:01 +0300

    half done or at least 4 tests

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,25 @@
 class Song:
+    count = 0
+    genres = ''
+    genre_count = []
+    artists = ''
+
+    def __init__(self, name, artist, genres):
+        self.name = name
+        self.artist = artist
+
+        self.genre = genres
+        type(self).count += 1
+        type(self).genres += self.genre
+        type(self).artists+= self.artist
+
+        self.add_song_to_count()
+
+
+    def add_song_to_count(self):
+        self.count = self.count + 1
+        return self.count
+
+    def genre_count(self):
+
     pass
